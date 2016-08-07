2decc0ce2ff02774948537f10ede0ba8c7f63a26
Neil Grogan <neil@ngrogan.com>
2016-08-07 20:00:48 +0100

    Added unit testing that actually works :)

--- test/test.js
+++ test/test.js
@@ -1,8 +1,21 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+import App from '../src/scripts/components/App';
+import Navbar from '../src/scripts/components/Navbar';
+import Carousel from '../src/scripts/components/Carousel';
+
+describe('<App />', () => {
+
+  it('contains an <Navbar/> component', function () {
+    const wrapper = mount(<App/>);
+    expect(wrapper.find(Navbar)).to.have.length(1);
   });
+
+  it('contains an <Carousel/> component', function () {
+    const wrapper = mount(<App/>);
+    expect(wrapper.find(Carousel)).to.have.length(1);
+  });
+
+
 });
